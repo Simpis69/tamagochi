ccd86a6fd9710eee07ba0529dcb1322ce44ab53b
Leonard wegmann simon <leonard.wegmannsimon@elev.ga.ntig.se>
2024-09-10 10:59:35 +0200

    öujqhbeföijb

---
+++ Tamagochi/Program.cs
@@ -0,0 +1,11 @@
+﻿weapon axe = new();
+//     Name = "axe",
+//     MinDamage = 10,
+//     MaxDamage = 20
+// };
+
+// int damage = axe.Attack();
+
+// Console.WriteLine(damage);
+
+Console.ReadLine();
---
+++ Tamagochi/Tamagochi.cs
@@ -0,0 +1,53 @@
+
+
+
+// public class tamagochi{
+//     private int Hunger;
+//     private int Boredom;
+//     private List<string> words = new List<string>() {"hello"};
+//     private bool isAlive;
+//     private Random generator;
+//     private string name;
+// }
+
+
+
+//   public Tamagotchi()
+//   {
+//     generator = new Random();
+//     isAlive = true;
+//   }
+
+
+
+// /*                                                  Publika klasser                                                                               */
+// /*---------------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+// public void Feed()
+// {
+    
+// }
+// public void Hi()
+// {
+
+// }
+// public void Teach(string words)
+// {
+
+// }
+
+// public void Tick()
+// {
+
+// }
+// public void PrintStats()
+// {
+
+// }
+// public bool GetAlive()
+// {
+
+// }
+
